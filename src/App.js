d2223f1b5a8d9003148ea10b71ee16c23765a0be
Tom Szwaja <th.szwaja@gmail.com>
2021-11-07 13:49:07 +0100

    chore: css setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Box, Plane, PerspectiveCamera } from "@react-three/drei";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="canvas-container">
+            <Canvas>
+                {/* <PerspectiveCamera
+                    makeDefault
+                    position={[0, 5, -15]}
+                    fov={45}
+                    aspect={window.innerWidth / window.innerHeight}
+                    near={1}
+                    far={1000}
+                /> */}
+                <ambientLight intensity={0.1} />
+                <directionalLight color="red" position={[0, 0, 5]} />
+                <Plane args={[20, 20]} />
+                <Box args={[5, 5, 5]}>
+                    <meshPhongMaterial attach="material" color="#f3f3f3" />
+                </Box>
+            </Canvas>
+        </div>
+    );
 }
 
 export default App;
